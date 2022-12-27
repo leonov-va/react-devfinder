a3ae02064edfb5d068de415d67b3449ac13c6fe4
Vladislav Leonov <leonov.xi@gmail.com>
2022-12-27 17:40:20 +0300

    create usertitle component

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -1,15 +1,27 @@
 import { UserStat } from "components/UserStat";
+import { UserTitle } from "components/UserTitle";
 import { LocalGithubUser } from "types";
 import styles from "./UserCard.module.scss";
 
 interface UserCardProps extends LocalGithubUser {}
 
-export const UserCard = (props: UserCardProps) => (
-  <div className={styles.userCard}>
-    <UserStat
-      repos={props.repos}
-      followers={props.followers}
-      following={props.following}
-    />
-  </div>
-);
+export const UserCard = (props: UserCardProps) => {
+  return (
+    <div className={styles.userCard}>
+      <img src={props.avatar} alt={props.name} className={styles.avatar} />
+      <UserTitle
+        created={props.created}
+        login={props.login}
+        name={props.name}
+      />
+      <p className={`${styles.bio}${props.bio ? "" : ` ${styles.empty}`}`}>
+        {props.bio || "This profile has no bio"}
+      </p>
+      <UserStat
+        repos={props.repos}
+        followers={props.followers}
+        following={props.following}
+      />
+    </div>
+  );
+};
--- src/components/UserStat/UserStat.tsx
+++ src/components/UserStat/UserStat.tsx
@@ -11,10 +11,14 @@ export const UserStat = ({ repos, followers, following }: UserStatProps) => (
         <div className={styles.infoTitle}>Repos</div>
         <div className={styles.infoNumber}>{repos}</div>
       </div>
+    </div>
+    <div className={styles.userStat}>
       <div className={styles.info}>
         <div className={styles.infoTitle}>Following</div>
         <div className={styles.infoNumber}>{following}</div>
       </div>
+    </div>
+    <div className={styles.userStat}>
       <div className={styles.info}>
         <div className={styles.infoTitle}>Followers</div>
         <div className={styles.infoNumber}>{followers}</div>
